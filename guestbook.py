25784ff04d901a5cb79123b28b5e929f9ad0efe8
Henry Edmeades <s3606607@student.rmit.edu.au>
2020-03-25 11:56:33 +1100

    working no 500

--- guestbook.py
+++ guestbook.py
@@ -40,27 +40,20 @@ DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 # will be consistent. However, the write rate should be limited to
 # ~1/second.
 
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+def user_key(user_id):
     """Constructs a Datastore key for a Guestbook entity.
 
     We use guestbook_name as the key.
     """
-    return ndb.Key('Guestbook', guestbook_name)
+    return ndb.Key('id', user_id)
 
 
 # [START greeting]
-class Author(ndb.Model):
-    """Sub model for representing an author."""
-    identity = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
-
-
-class Greeting(ndb.Model):
+class User(ndb.Model):
     """A main model for representing an individual Guestbook entry."""
-    author = ndb.StructuredProperty(Author)
-    content = ndb.StringProperty(indexed=False)
-    country = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
+    id = ndb.KeyProperty(indexed=True)
+    name = ndb.StringProperty(indexed=False)
+    password = ndb.IntegerProperty(indexed=False)
 # [END greeting]
 
 
@@ -68,34 +61,11 @@ class Greeting(ndb.Model):
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-
-        #sourced from https://stackoverflow.com/questions/18649512/unicodedecodeerror-ascii-codec-cant-decode-byte-0xe2-in-position-13-ordinal
-        url = "https://gist.githubusercontent.com/marijn/396531/raw/188caa065e3cd319fed7913ee3eecf5eec541918/countries.txt"
-
-        response = urllib.urlopen(url)
-        countries = response.readlines()
-
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greetings_query = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
-
-        user = users.get_current_user()
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
+        login_failed = self.request.get('login_fail',
+                                          False)
 
         template_values = {
-            'user': user,
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-            'countries': countries
+            'login_failed': login_failed
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
@@ -104,7 +74,7 @@ class MainPage(webapp2.RequestHandler):
 
 
 # [START guestbook]
-class Guestbook(webapp2.RequestHandler):
+class Login(webapp2.RequestHandler):
 
     def post(self):
         # We set the same parent key on the 'Greeting' to ensure each
@@ -112,27 +82,42 @@ class Guestbook(webapp2.RequestHandler):
         # single entity group will be consistent. However, the write
         # rate to a single entity group should be limited to
         # ~1/second.
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
 
-        if users.get_current_user():
-            greeting.author = Author(
-                    identity=users.get_current_user().user_id(),
-                    email=users.get_current_user().email())
+        user_query = User.query(
+                    ancestor=user_key(self.request.get('username')))
+        user = user_query.fetch(1)
 
-        greeting.content = self.request.get('content')
-        greeting.country = self.request.get('country')
-        greeting.put()
+        login_failed = True
+        found_user = None
+        if len(user) == 1:
+            found_user = user[0]
+            if found_user.password == self.request.get('password'):
+                login_failed = False
 
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
+
+        #login_request = User(parent=user_key(self.request.get('username'))
+
+        #login_request.username = self.request.get('username')
+        #login_request.password = self.request.get('password')
+        #login_request.put()
+
+        template_values = {
+            'response': user
+        }
+
+      #  template = JINJA_ENVIRONMENT.get_template('index.html')
+       # self.response.write(template.render(template_values))
+
+        if login_failed:
+            self.redirect('/?' + urllib.urlencode({'login_fail': found_user}))
+        else:
+            self.redirect('/main?' + urllib.urlencode({'username': self.request.get('username')}))
 # [END guestbook]
 
 
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign', Guestbook),
+    ('/login', Login),
 ], debug=True)
 # [END app]
